ec480dee6d857f7a64c3a3a9215663024b672ac5
sarakhaled95 <architectsarakhaled@outlook.com>
2023-09-25 04:53:54 -0700

    3-main.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    value = 0
+    try:
+        value = a / b
+    except ZeroDivisionError:
+        value = None
+    finally:
+        print("Inside result: {} ".format(value))
+        return value
