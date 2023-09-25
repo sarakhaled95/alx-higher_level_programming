9f5a3f933af7b2eee3047963b73cc60332f71e76
sarakhaled95 <architectsarakhaled@outlook.com>
2023-09-25 05:40:10 -0700

    5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def raise_exception():
     try:
-        raise Exception('Exception raised')
-    except Exception as error:
+        raise TypeError
+    except TypeError as error:
         print("Exception raised")
