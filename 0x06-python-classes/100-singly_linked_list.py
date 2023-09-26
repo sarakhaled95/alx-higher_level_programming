9de70834979b4ce9e7758f142ca4966a861ee775
sarakhaled95 <architectsarakhaled@outlook.com>
2023-09-26 09:30:03 -0700

    100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ node module"""
 
+
 class Node:
     """defines a node of a singly linked list"""
 
@@ -31,13 +32,13 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if not isinstance(value, Node) and value != None:
+        if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.next_node = value
 
+
 class SinglyLinkedList:
     """ defines a singly linked list"""
-
 
     def __init__(self):
         """initialize a new SinglyLinkedList"""
@@ -51,15 +52,15 @@ class SinglyLinkedList:
             value (Node): the new Node to insert
         """
         new = Node(value)
-        if self.__head == None:
+        if self.__head is None:
             new.next_node = None
             self.__head = new
-        elif self.__head.data->value:
+        elif self.__head.data-> value:
             new.next_node = self.__head
             self.__head = new
-        else
+        else:
             temp = self.__head
-            while (temp.next_node != None and
+            while (temp.next_node is not None and
                     temp.next_node < value):
                 temp = temp.next_node
             new.next_node = temp.next_node
@@ -69,7 +70,7 @@ class SinglyLinkedList:
         """define the print() representation"""
         values = []
         temp = self.__head
-        while temp != None:
+        while temp is not None:
             value.append(str(temp.data))
             temp = temp.next_node
         return ('\n'.join(values))
