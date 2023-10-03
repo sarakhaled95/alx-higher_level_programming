f9084525e22b34078d6d1eb769201d336c4ec0f7
sarakhaled95 <architectsarakhaled@outlook.com>
2023-10-03 18:00:32 +0300

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def magic_string(n):
-    for i in range(n):
-        return "BestSchool" * i 
+def magic_string():
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_sting.count)])
 
