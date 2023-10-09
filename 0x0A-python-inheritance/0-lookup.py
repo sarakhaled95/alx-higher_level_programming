179a74085c411be95be90c7b023914a785481ba5
sarakhaled95 <architectsarakhaled@outlook.com>
2023-10-09 06:07:04 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" lookup module """
+
+
+def lookup(obj):
+    """function that returns the list of available attributes
+    and methods of an object
+
+    Args:
+        obj: object
+
+    Returns:
+        list of available attributes
+        and methods of an object
+    """
+    return dir(obj)
