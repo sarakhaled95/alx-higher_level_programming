814d75d1e23304ab6bda0c90a225122072308d16
sarakhaled95 <architectsarakhaled@outlook.com>
2023-10-10 09:29:58 -0700

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" student class module"""
+
+
+class Student:
+    """defines student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dict representation of student
+        If attrs is a list of strings, only attribute
+        names contained in this list must be retrieved.
+        """
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance"""
+
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
