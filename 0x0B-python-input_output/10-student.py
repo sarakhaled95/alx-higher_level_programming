de3ae9db9db68ebbc7d5671a60577565c40c1474
sarakhaled95 <architectsarakhaled@outlook.com>
2023-10-10 09:22:31 -0700

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" student class module"""
+
+
+class Student:
+    """defines student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dict representation of student
+        If attrs is a list of strings, only attribute
+        names contained in this list must be retrieved.
+        """
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        for key, value in self.__dict__.item():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
