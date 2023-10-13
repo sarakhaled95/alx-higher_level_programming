90e3ba80d7a6b7fb6e5f24a8fdab6f18184892ec
sarakhaled95 <architectsarakhaled@outlook.com>
2023-10-13 06:56:11 -0700

    11-main.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,3 +22,21 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """internal update method"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """update method"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
