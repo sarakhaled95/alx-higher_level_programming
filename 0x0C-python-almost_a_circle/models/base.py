6c90cb4b06c70652f6e53a0553084f7c0e1f91ac
sarakhaled95 <architectsarakhaled@outlook.com>
2023-10-15 02:09:39 -0700

    16-main.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,3 +29,10 @@ class Base:
             list_objs = [o.to_dictionary() for o in list_objs]
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """return the list of the json string representation"""
+        if json_string is None or not json_string:
+            return "[]"
+        return json.loads(json_string)
