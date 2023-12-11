33eef9e3f5f6cacf43c2f66a62579677a5b1e707
sarakhaled95 <architectsarakhaled@outlook.com>
2023-12-11 18:27:35 +0200

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 switch (args.length) {
-    case
-        0:
-        console.log("No argument");
-        break;
-    case
-        1:
-        console.log("Argument found");
-        break;
-    default:
-        console.log("Argument found");
+  case
+    0:
+    console.log("No argument");
+    break;
+  case
+    1:
+    console.log("Argument found");
+    break;
+  default:
+    console.log("Argument found");
 }
