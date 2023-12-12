421061e2cc6c5f5baeb31188e35b132cae263f10
sarakhaled95 <architectsarakhaled@outlook.com>
2023-12-12 21:17:56 +0200

    3

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
+const dict = require('./101-data');
 
+const wholeList = Object.entries(dict);
+const val = Object.values(dict);
+const uniqeVal = [...new Set(val)];
+const newDict = {};
+for (const i in uniqeVal) {
+  const list = [];
+  for (const j in wholeList) {
+    if (wholeList[j][1] === uniqeVal[i]) {
+      list.unshift(wholeList[j][0]);
+    }
+  }
+  newDict[uniqeVal[i]] = list;
+}
+console.log(newDict);
