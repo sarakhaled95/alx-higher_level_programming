1cb163e397e34343ccd9aa59e750de62f5140b34
sarakhaled95 <architectsarakhaled@outlook.com>
2023-12-12 18:22:11 +0200

    4-rec

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (h, w) {
+    if (h > 0 && w > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.width) {
+      console.log('X'.repeat(this.height));
+      i++;
+    }
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+
+    rotate() {
+        const exch = this.width;
+        this.width = this.hight;
+        this.hight = exch;
+    }
+
+}
+module.exports = Rectangle;
