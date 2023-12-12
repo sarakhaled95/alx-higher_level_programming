327dda38ba31c93678374b8385143396590854d6
sarakhaled95 <architectsarakhaled@outlook.com>
2023-12-12 16:51:01 +0200

    0-rec

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (height, width) {
-    this.height = height;
-    this.width = width;
-  }
-}
-module.exports = Rectangle;
+
+module.exports = class Rectangle { };
