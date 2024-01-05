20db470349c94d73a9249295bd5d37cdc55bae11
sarakhaled95 <architectsarakhaled@outlook.com>
2024-01-05 05:26:07 -0800

    1-body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # displays the body of the response
-if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ];
-then curl -Ls "$1";
-fi
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
