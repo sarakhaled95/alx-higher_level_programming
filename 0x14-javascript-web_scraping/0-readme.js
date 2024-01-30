d928993a34ad36ad2e4f3af0d8636ed2b8553bf7
sarakhaled95 <architectsarakhaled@outlook.com>
2024-01-30 07:10:11 -0800

    0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file.
+const fs = require('fs');
+const process = require('process');
+
+
+const args = process.argv.slice(2);
+
+fs.readFile(args[0], (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data.toString());
+	}
+});
