123d4a24558d636df9dac2eac5be85d7295f18a5
sarakhaled95 <architectsarakhaled@outlook.com>
2024-01-30 09:19:09 -0800

    6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// script that computes the number of tasks completed by user id.
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const complete = {};
+    const tasks = JSON.parse(body);
+    let i = 0;
+    for (i = 0; i < tasks.length; i++) {
+      if (!(tasks[i].userId in complete)) {
+        complete[tasks[i].userId] = 0;
+      }
+      if (tasks[i].completed) {
+        complete[tasks[i].userId] += 1;
+      }
+      if (complete[tasks[i].userId] === 0) {
+        delete complete[tasks[i].userId];
+      }
+    }
+    console.log(complete);
+  }
+});
