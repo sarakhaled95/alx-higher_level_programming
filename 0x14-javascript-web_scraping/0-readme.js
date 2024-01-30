6e030cf8f306a0c3554196000d9007aa0224183d
sarakhaled95 <architectsarakhaled@outlook.com>
2024-01-30 07:13:48 -0800

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,13 +3,12 @@
 const fs = require('fs');
 const process = require('process');
 
-
 const args = process.argv.slice(2);
 
 fs.readFile(args[0], (err, data) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(data.toString());
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
 });
