37051ccbce6276c1d90faedc6fa00b4fcdfab887
sarakhaled95 <architectsarakhaled@outlook.com>
2024-02-07 17:54:01 +0200

    4 5 and 6

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,5 @@
+//updates the text color of the <header> element to red (#FF0000)
+//when the user clicks on the tag DIV#red_header:
+$('#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+});
---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+//updates the text color of the <header> element to red (#FF0000)
+//when the user clicks on the tag DIV#red_header:
+$('##update_header').click(function () {
+    $('header').text('New Header!!!');
+});
